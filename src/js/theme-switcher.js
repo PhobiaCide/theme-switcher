a04b376e9b33cba26a88edd7bdee97bb2c8ea1ab
PhobiaCide <aamason86@gmail.com>
2024-02-26 16:32:44 -0600

    * feat(theme-switcher): refactor theme-switcher.js and theme-switcher1.js for improved functionality and readability

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -1,75 +1,53 @@
 // Constants and Destructuring
-const { body, documentElement } = document;
+const { body, documentElement, getElementsByName } = document;
 const widthBreakpoint = 768;
 
 // Local Storage Functions
-const storeTheme = (string) => localStorage.setItem("theme", string);
-const getStoredTheme = () => localStorage.getItem("theme") || null;
+const saveSelectedTheme = (string) => localStorage.setItem("theme", string);
+const getSavedTheme = () => localStorage.getItem("theme");
 
 // Theme-related Functions
-const themeElements = document.getElementsByName("theme");
+const getThemeElements = () => [...document.getElementsByName("theme")];
+const getSelectedTheme = () => getThemeElements()?.filter(element => element?.checked)[0]?.value;
 
-const getSelectedTheme = () => {
-  const selectedThemeElement = Array.from(themeElements).find((element) => element.checked);
-  return selectedThemeElement ? selectedThemeElement.value : null;
-};
 
-const activateTheme = () => {
-  const theme = getSelectedTheme();
-  documentElement.setAttribute("data-theme", theme);
-  storeTheme(theme);
+const applyTheme = (theme) => {
+  const attribute = "data-theme";
+  documentElement.setAttribute(attribute, theme);
+  saveSelectedTheme(theme);
 };
 
 // Theme Mode Functions
-const determineMode = () => (parseInt(documentElement.getAttribute("data-theme"), 10) >= 5) ? "dark" : "light";
+const getThemeMode = () => {
+  const mode = documentElement.getAttribute("data-theme");
+  const isDark = () => (mode >= 5);
+
+  return isDark() ? "dark" : "light";
+};
 
 // Initialization
-const applyMode = () => document.documentElement.setAttribute("data-bs-theme", determineMode());
+const applyMode = (mode) => {
+  const attribute = "data-bs-theme";
+  document.documentElement.setAttribute(attribute, mode);
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-  applyMode();
-  donTopcoat();
+  const savedTheme = getSavedTheme();
+
+  if (savedTheme)
+  {
+    applyTheme(savedTheme);
+  } else
+  {
+    // If no saved theme, use the user's preferred color scheme
+    applyMode(getThemeMode());
+  }
 
   // Event listener for theme selection
-  themeElements.forEach((element) => {
+  document.getElementsByName("theme").forEach((element) => {
     element.addEventListener("click", () => {
-      activateTheme();
-      applyMode();
+      applyTheme(getSelectedTheme());
+      applyMode(getThemeMode());
     });
   });
 });
-
-// Topcoat Functions
-const getScreenSize = () => (window.innerWidth >= widthBreakpoint) ? 'desktop' : 'mobile';
-
-const constructTopcoatHref = (themeMode) => `https://cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/css/topcoat-${getScreenSize()}-${themeMode}.min.css`;
-
-const donTopcoat = () => {
-  const activeMode = document.documentElement.getAttribute('data-bs-theme').valueOf();
-  console.log(`Active mode: ${activeMode}`);
-
-  const href = constructTopcoatHref(activeMode);
-  console.log(`Topcoat href: ${href}`);
-
-  const topcoat = document.getElementById('topcoat-stylesheet');
-  console.log(`Topcoat element: ${topcoat ? topcoat.id : 'Not found'}`); // Log the id property of the topcoat element if found
-
-  if (topcoat) {
-    topcoat.setAttribute('href', href);
-    applyMode();
-  }
-};
-
-window.addEventListener('resize', donTopcoat);
-
-// Create a MutationObserver instance
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (mutation.attributeName === 'data-theme') {
-      donTopcoat();
-    }
-  });
-});
-
-// Configure and start the observer
-observer.observe(document.documentElement, { attributes: true, attributeFilter: ['data-theme'] });
